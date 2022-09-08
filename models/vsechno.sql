3daa4c7b50b625878a4c1bba25000581ab5790e2
lubos.svoboda@revolt.bi <lubos.svoboda@revolt.bi>
2022-09-08 07:46:41 +0000

    casting removed

--- models/vsechno.sql
+++ models/vsechno.sql
@@ -3,11 +3,7 @@
 
 select 
 cast(md5(concat (hospoda,mesto)) as text) as ukey,
-cast(hospoda as text),
-cast(mesto as text),
-cast(pivo as text),
-cast(cena as int),
-cast(aktualizace as timestamp)
+*
  from {{ ref('scd') }} 
 where aktualizace < '2022-02-01'
 -- where aktualizace < '2021-01-01'
--- snapshots/mintest1.sql
+++ snapshots/mintest1.sql
@@ -3,18 +3,19 @@
 
 {{
     config(
-      target_schema='snapshots',
+      target_schema='dbt_lubos',
        unique_key='user_id',
        strategy='check',
         check_cols='all'    )
 }}
 
--- select 1 as user_id, 'inactive' as status, 123 as price
--- union all
-select 2 as user_id, 'inactive' as status, 123 as price
+select 1 as user_id, 'inactive' as status, 123 as price
 union all
-select 2 as user_id, 'active' as status, 123 as price
-
+select 2 as user_id, 'inactive' as status, 123 as price
+-- union all
+-- select 2 as user_id, 'active' as status, 123 as price
+-- union all
+-- select 2 as user_id, 'active' as status, 999 as price
 {% endsnapshot %}
 
 
